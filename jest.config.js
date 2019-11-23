cda6a4b94e65069f65e0a97605c8d76c75ec3c8e
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-11-22 21:56:03 -0500

    chore: Updates jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
+const path = require("path")
+
 module.exports = {
-	setupFiles: ["./jest.setup.js"]
+  setupFiles: ["./jest.setup.js"],
+  moduleDirectories: [
+    path.resolve("./src"),
+    "node_modules",
+  ],
+  moduleFileExtensions: ["js", "jsx", "json",],
+  moduleNameMapper: {
+    "^@tests(.*)$": "<rootDir>/tests/$1",
+    "^@src(.*)$": "<rootDir>/src/$1",
+  },
+  testRegex: "((/test/(integration/unit).js)|(test.js))$"
 }
