888ff6e19766578b734cdf7e97bd8bb1ec600b5a
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-12-18 20:08:03 -0500

    test: Updates test.

--- tests/simple-input-error/simple-input-error.test.js
+++ tests/simple-input-error/simple-input-error.test.js
@@ -5,25 +5,14 @@ import { SimpleInputError } from "src"
 const message = "This is an error message"
 
 describe("SimpleInputError", () => {
-  it("should render an error when isVisible is true", () => {
+  it("should render an error message", () => {
     const wrapper = mount(
       <SimpleInputError
-        isVisible={true}
         message={message}
         position={"centerLeft"}
       />
     )
     expect(wrapper.find(SimpleInputError)).toHaveLength(1)
     expect(wrapper.find("div.SimpleInputError-Message").text()).toEqual(message)
-  })
-  it("should render nothing when isVisible is false", () => {
-    const wrapper = mount(
-      <SimpleInputError
-        isVisible={false}
-        message={message}
-        position={"centerLeft"}
-      />
-    )
-    expect(wrapper.find("div.SimpleInputError")).toHaveLength(0)
   })
 })
